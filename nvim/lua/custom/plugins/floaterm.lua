6cb36844dda98fcec839bf4278f73ff59a34521b
Po Lo <po.locp@shopee.com>
2024-12-24 10:10:06 +0800

    WE STRONK, WE APE

--- nvim/lua/custom/plugins/floaterm.lua
+++ nvim/lua/custom/plugins/floaterm.lua
@@ -2,13 +2,17 @@ return {
   'voldikss/vim-floaterm',
   event = 'VeryLazy',
   config = function()
-    -- Set the width and height of the Floaterm window
+    vim.g.floaterm_title = '🦍🦍🦍🦍 WE STRONK 🦍🦍🦍🦍 WE APE 🦍🦍🦍🦍'
     vim.g.floaterm_width = 0.99
-    vim.g.floaterm_height = 0.4
+    vim.g.floaterm_height = 0.7
     vim.g.floaterm_position = 'bottom'
+    vim.g.floaterm_titleposition = 'center'
+    vim.g.floaterm_borderchars = '─│─│╭╮╯╰'
 
     -- Key mappings for normal mode and terminal mode
-    vim.api.nvim_set_keymap('n', '<leader>j', ':FloatermToggle<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<C-j>', ':FloatermToggle<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('t', '<C-j>', '<C-\\><C-n>:FloatermToggle<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>j', ':FloatermToggle<CR>', { noremap = true, silent = true, desc = "[J]FloatermToggle"})
     vim.api.nvim_set_keymap('t', '<leader>j', '<C-\\><C-n>:FloatermToggle<CR>', { noremap = true, silent = true })
   end,
 }
