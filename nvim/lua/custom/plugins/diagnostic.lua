e9c17f2ff7c5a41f74c8b5818121ddc12d640307
Po Lo <po.locp@shopee.com>
2025-01-08 10:08:54 +0800

    rename plugin

---
+++ nvim/lua/custom/plugins/diagnostic.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'folke/trouble.nvim',
+    cmd = 'Trouble',
+    keys = {
+      { '<leader>tt', '<cmd>Trouble diagnostics toggle<cr>', desc = '[T]rouble [T]oggle' },
+      { '<leader>tq', '<cmd>Trouble quickfix<cr>', desc = '[T]rouble [Q]uickfix' },
+    },
+    opts = {
+      action_key = {
+        use_diagnostic_signs = true,
+        action_keys = {
+          close = { 'q', '<esc>' },
+        },
+      },
+    },
+  },
+}
