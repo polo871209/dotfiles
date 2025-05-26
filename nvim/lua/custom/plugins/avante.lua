e32658582b94be83fb2a453dbd68f16c04402189
PoLo <qazh0123@gmail.com>
2025-05-26 14:33:45 +0800

    remove image clip and use claude

--- nvim/lua/custom/plugins/avante.lua
+++ nvim/lua/custom/plugins/avante.lua
@@ -17,8 +17,8 @@ return {
       -- for example
       provider = 'copilot',
       copilot = {
-        -- model = 'claude-sonnet-4',
-        model = 'gemini-2.5-pro',
+        model = 'claude-sonnet-4',
+        -- model = 'gemini-2.5-pro',
         -- timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
         temperature = 0,
         -- max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
@@ -44,23 +44,6 @@ return {
       'ibhagwan/fzf-lua', -- for file_selector provider fzf
       'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
       'zbirenbaum/copilot.lua', -- for providers='copilot'
-      {
-        -- support for image pasting
-        'HakonHarnes/img-clip.nvim',
-        event = 'VeryLazy',
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
       {
         -- Make sure to set this up properly if you have lazy=true
         'MeanderingProgrammer/render-markdown.nvim',
--- nvim/lua/custom/plugins/snacks.lua
+++ nvim/lua/custom/plugins/snacks.lua
@@ -8,7 +8,6 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    image = { enabled = true },
     indent = { enabled = true },
     lazygit = { configure = false },
     notifier = {
