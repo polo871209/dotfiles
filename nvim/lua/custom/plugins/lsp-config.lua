1c88db2cdd2c495dfeb1c0ae849a969cc1960f4a
PoLo <qazh0123@gmail.com>
2025-08-17 15:20:18 +0800

    bitwarden script
    Signed-off-by: PoLo <qazh0123@gmail.com>

--- nvim/lua/custom/plugins/lsp-config.lua
+++ nvim/lua/custom/plugins/lsp-config.lua
@@ -163,6 +163,7 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         bashls = {},
+        gopls = {},
         jsonls = {},
         pyrefly = {},
         ruff = {},
