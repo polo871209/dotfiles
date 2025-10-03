9928eb434422307222da776ce2890c786ee89f81
PoLo <qazh0123@gmail.com>
2025-10-03 13:39:21 +0800

    fix: trouble
    Signed-off-by: PoLo <qazh0123@gmail.com>

--- nvim/lua/custom/plugins/diagnostic.lua
+++ nvim/lua/custom/plugins/diagnostic.lua
@@ -1,10 +1,11 @@
 return {
   {
     'folke/trouble.nvim',
+    opts = {},
     cmd = 'Trouble',
     keys = {
-      { '<leader>tt', '<cmd>Trouble diagnostics toggle<cr>', desc = '[T]rouble [T]oggle' },
-      { '<leader>tq', '<cmd>Trouble quickfix<cr>', desc = '[T]rouble [Q]uickfix' },
+      { '<leader>tt', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = '[T]rouble [T]oggle' },
+      { '<leader>tq', '<cmd>Trouble qflist toggle<cr>', desc = '[T]rouble [Q]uickfix' },
     },
   },
 }
