342cc0792136fc1ba52f34b2a34baad355d6e5d0
PoLo <qazh0123@gmail.com>
2025-11-09 15:54:37 +0800

    feat: consistant background color and some cleanup
    Signed-off-by: PoLo <qazh0123@gmail.com>

--- nvim/lua/custom/plugins/style.lua
+++ nvim/lua/custom/plugins/style.lua
@@ -1,34 +1,23 @@
 return {
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'catppuccin/nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    priority = 1000,
     config = function()
-      -- Configure Catppuccin with transparent background
       require('catppuccin').setup({
         transparent_background = true,
         custom_highlights = function(colors)
           return {
-            yamlKey = { fg = colors.blue },
-            yamlBlockMappingKey = { fg = colors.blue },
-            yamlString = { fg = colors.green },
-            yamlPlainScalar = { fg = colors.text },
-            yamlFlowString = { fg = colors.green },
-            yamlBlockScalar = { fg = colors.green },
+            -- Gruvbox Dark background for popup windows for consistancy
+            NormalFloat = { bg = '#282828' },
+            FloatBorder = { bg = '#282828', fg = '#fabd2f' },
           }
         end,
       })
     end,
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme('catppuccin-mocha')
 
-      -- You can configure highlights by doing something like:
+      -- Remove highlight from comments
       vim.cmd.hi('Comment gui=none')
     end,
   },
@@ -39,6 +28,7 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = { signs = false },
   },
+  -- Highlight text matching the current selection in visual mode
   {
     'wurli/visimatch.nvim',
     opts = {},
@@ -46,5 +36,5 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     ft = { 'markdown', 'codecompanion' },
-  },
+  }
 }
