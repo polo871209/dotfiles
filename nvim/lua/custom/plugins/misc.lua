d1ee78e904ab5a4fb2f21e3d565f766e6e3dc35e
PoLo <qazh0123@gmail.com>
2026-03-15 12:09:19 +0800

    some improvnment

--- nvim/lua/custom/plugins/misc.lua
+++ nvim/lua/custom/plugins/misc.lua
@@ -1,4 +1,5 @@
 return {
+  --  Navigation & Motion
   {
     'christoomey/vim-tmux-navigator',
     cmd = {
@@ -14,17 +15,18 @@ return {
       { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
     },
   },
-  { 'ekalinin/Dockerfile.vim' },
   {
     'folke/flash.nvim',
     event = 'VeryLazy',
     opts = {},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    },
   },
-  },
+
+  -- UI & Experience
   {
     'folke/which-key.nvim',
     event = 'VimEnter',
@@ -42,6 +44,49 @@ return {
     },
   },
   {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+    },
+    opts = {
+      presets = {
+        bottom_search = true,
+      },
+      cmdline = {
+        enabled = true,
+        view = 'cmdline_popup',
+      },
+      views = {
+        cmdline_popup = {
+          position = { row = 3, col = '50%' },
+        },
+      },
+      -- Disable everything else to prevent behavior changes
+      messages = { enabled = false },
+      popupmenu = { enabled = false },
+      notify = { enabled = false },
+      lsp = {
+        progress = { enabled = false },
+        hover = { enabled = false },
+        signature = { enabled = false },
+        message = { enabled = false },
+      },
+    },
+  },
+  {
+    'stevearc/quicker.nvim',
+    ft = 'qf',
+    opts = {},
+  },
+
+  -- Coding & Editing Support
+  { 'windwp/nvim-autopairs', event = 'InsertEnter', opts = {} },
+  { 'NMAC427/guess-indent.nvim', opts = {} },
+  { 'ekalinin/Dockerfile.vim' },
+
+  -- Version Control (Git)
+  {
     'lewis6991/gitsigns.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
     opts = {
@@ -54,19 +99,8 @@ return {
       },
     },
   },
-  { 'NMAC427/guess-indent.nvim', opts = {} },
   {
     'sindrets/diffview.nvim',
     cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles' },
-  },
-  {
-    'stevearc/quicker.nvim',
-    ft = 'qf',
-    opts = {},
-  },
-  {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    opts = {},
   },
 }
